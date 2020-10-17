cd86e791adbb6f86c51543f55afa232a8aaf8bd7
Nicolas Cuevas <nicolasgcuevas@gmail.com>
2020-10-17 19:12:19 -0300

    javascript ejercicios inicial

---
+++ 2-InstruccionIf/jsInstruccionIF (11).js
@@ -0,0 +1,14 @@
+function Mostrar()
+{
+	var decision;
+	decision=confirm("Desea continuar?");
+	if(decision)
+	{
+		alert("eligio Aceptar");
+	}	
+	else
+	{
+		alert("eligio Cancelar");
+	}
+
+}//FIN DE LA FUNCIÓN
